022171e80d1422d6c72439d545bd1e5d007aeabd
metalels <metalels86@gmail.com>
2017-02-17 11:17:28 +0900

    Fixes support 1.9.x

--- lib/diff_influence.rb
+++ lib/diff_influence.rb
@@ -1,7 +1,7 @@
 require "diff_influence/version"
 require "diff_influence/config"
 
-if RUBY_VERSION < "1.9.0"
+if RUBY_VERSION < "2.0.0"
   require "diff_influence/core_1x"
 else
   require "diff_influence/core"
